c106d1819b649100eefd868bc6c73574a2b826cc
EvgenyFomin <sevenup96@mail.ru>
2019-02-01 22:56:57 +0300

    b12

--- src/B1.java
+++ src/B1.java
@@ -2,5 +2,6 @@ public class B1 {
     public static void main(String[] args) {
         System.out.println("Hello B1");
         System.out.println("Hello B12");
+        System.out.println("Hello B14");
     }
 }
