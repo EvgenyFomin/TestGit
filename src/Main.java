e2b0c45ef20baaddece3a8a32cfa6c8d62f5e0ed
EvgenyFomin <sevenup96@mail.ru>
2019-02-17 22:06:59 +0300

    3commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World2");
-        System.out.println("Hello World HW5");
-        System.out.println("Hello World HW5");
-        System.out.println("Hello World HW5");
-        System.out.println("Hello World4");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
