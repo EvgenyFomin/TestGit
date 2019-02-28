f37197a83a23b3eff440c8cbbf86a3cf42ff8e52
EvgenyFomin <sevenup96@mail.ru>
2019-03-01 00:41:32 +0300

    someChanges

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 public class Main {
+    static {
+        System.out.println("static");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello World");
